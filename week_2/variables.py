2c755bdd0e1ac0ea281b576a1a501a8fd757ade6
TKAYHERAWESOMENESS <160044276+TKAYHERAWESOMENESS@users.noreply.github.com>
2024-02-21 01:21:12 -0800

    Add files via upload

---
+++ week_2/variables.py
@@ -0,0 +1,48 @@
+# Data types in python
+#Date:21/02/24
+#Name: Tracy Kanyora
+
+# Integers are positive or negative whole numbers
+
+x= 10
+y= -24
+
+
+# float are decimal/ floating points
+
+pi = 3.142
+
+
+
+# character : alphanumerc , 0-9 and a-z
+
+
+
+#special characters:# $ % ^ & *()
+grade = 'A' # use single quatation marks
+
+
+# string a group of characters
+
+f_name = "Kanyora" # you can use double or single quotes
+f_name = ' Kanyora'
+
+
+#naming conventions
+# camel case   nameOfCity --> the  first letter is a small letter but the rest is capital
+# snake case  name_of_city -->all are lower case but we use _(underscore) to separate the words
+
+#variables values that change when using
+h=20
+print ("h")
+h=20**2
+print("h")
+
+
+
+# constants they remain the same = dont change eg pi =(3.142)
+
+avogadrosConstant = 6.029* (10**23)
+
+
+                            
