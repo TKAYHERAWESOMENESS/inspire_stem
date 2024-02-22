fa46267908cb0ebcea661756c24f3063fe49a7a9
TKAYHERAWESOMENESS <160044276+TKAYHERAWESOMENESS@users.noreply.github.com>
2024-02-21 21:48:17 -0800

    Add files via upload

---
+++ employee records.py
@@ -0,0 +1,87 @@
+# This is a program that calculates employees salary and bonuses
+#Date: 21/02/2024
+# Name: Tracy Kanyora
+
+
+print("---EMPLOYEE REGISTER---")
+print(" 1. Rose Nyambura")
+print("2. Leah Wambui")
+print("3. Trevor John")
+print("-----")
+
+name = input("Enter employee name")
+
+print ("Name:", name)
+if name == "Rose Nyambura":
+age=30
+sal=40000
+bon=7500
+tot= sal +bon
+
+
+print("age:" , age)
+print("salary:" , sal)
+print("bonus:" , bon)
+print("total:" , tot)
+
+print("------")
+
+print ("due to revision")
+new_sal = sal = (0.3 * sal)
+new_bon = bon - 5000
+new_tot = new_sal + new_bon
+
+print("new salary:" , new_sal)
+print("new bonus:" , new_bon)
+print("new total:" , new_tot)
+
+elif name =="Leah Wambui":
+age=25
+sal=30000
+bon=8500
+tot= sal +bon
+
+print("age:" , age)
+print("salary:" , sal)
+print("bonus:" , bon)
+print("total:" , tot)
+
+print("-----")
+      
+print ("due to revision")
+new_sal = sal = (0.3 * sal)
+new_bon = bon - 5000
+new_tot = new_sal + new_bon
+
+print("new salary:" , new_sal)
+print("new bonus:" , new_bon)
+print("new total:" , new_tot)
+
+
+elif name == "Trevor John":
+age=28
+sal=38000
+bon=10500
+tot= sal +bon
+
+print("age:" , age)
+print("salary:" , sal)
+print("bonus:" , bon)
+print("total:" , tot)
+
+print("-----")
+print ("due to revision")
+new_sal = sal = (0.3 * sal)
+new_bon = bon - 5000
+new_tot = new_sal + new_bon
+
+print("new salary:" , new_sal)
+print("new bonus:" , new_bon)
+print("new total:" , new_tot)
+
+
+
+
+
+
+
